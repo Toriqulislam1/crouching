88552c547c074250f8659a1098080c9d7382e598
Toriqul Islam <96937038+Toriqulislam1@users.noreply.github.com>
2024-11-24 13:07:40 +0600

    Update create.blade.php
    rtwe

--- resources/views/admin/batch/create.blade.php
+++ resources/views/admin/batch/create.blade.php
@@ -31,7 +31,7 @@
                             </div>
                         </div>
                         <div class="card-footer">
-                            <button type="submit" class="btn btn-primary float-right">Add Batch</button>
+                            <button type="submit" class="btn btn-primary float-right">Add </button>
                         </div>
                     </form>
                 </div>
